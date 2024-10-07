4331572d55e9c6ddda36ba3f4c9d7a42ea010057
janneesa <janne.savinainen@hotmail.com>
2024-10-07 19:45:38 +0300

    refactoroitu OmaMoottori.java

--- src/main/java/simu/model/OmaMoottori.java
+++ src/main/java/simu/model/OmaMoottori.java
@@ -8,40 +8,11 @@ import eduni.distributions.Normal;
 public class OmaMoottori extends Moottori {
 
     private Saapumisprosessi saapumisprosessi;
-
-    // Todennäköisyys, että asiakas menee itsepalvelu check-iniin
-    private double selfCheckInTodennakoisyys = 0.5; // 50%
-
-    private Palvelupiste[] checkInPisteet;
-    private Palvelupiste[] selfCheckInPisteet;
-    private Palvelupiste[] turvatarkastusPisteet;
-    private Palvelupiste[] porttiPisteet;
-
-    // Palvelupisteiden palveluajat
-    private int meanCheckIn;
-    private int varianceCheckIn;
-    private int meanSelfCheckIn;
-    private int varianceSelfCheckIn;
-    private int meanTurvatarkastus;
-    private int varianceTurvatarkastus;
-    private int meanPortti;
-    private int variancePortti;
-    /*
-    private int meanPalveluaika;
-    private int variancePalveluaika;
-    */
-
-    // Saapumisprosessin saapumisväli
-    private int meanSaapumisvali;
-    private int varianceSaapumisvali;
-
-    // Palavelupisteiden asiakasmäärät
-    private int checkInAsiakasMaara;
-    private int selfCheckInAsiakasMaara;
-    private int turvatarkastusAsiakasMaara;
-    private int porttiAsiakasMaara;
-
-    private double prosessiAika = 0.0;
+    private double selfCheckInTodennakoisyys = 0.5;
+    private Palvelupiste[] checkInPisteet, selfCheckInPisteet, turvatarkastusPisteet, porttiPisteet;
+    private int meanCheckIn, varianceCheckIn, meanSelfCheckIn, varianceSelfCheckIn, meanTurvatarkastus, varianceTurvatarkastus, meanPortti, variancePortti;
+    private int meanSaapumisvali, varianceSaapumisvali;
+    private int checkInAsiakasMaara, selfCheckInAsiakasMaara, turvatarkastusAsiakasMaara, porttiAsiakasMaara;
 
     public OmaMoottori(IKontrolleriForM kontrolleri, int checkInKoko, int selfCheckInKoko, int turvatarkastusKoko, int porttiKoko, int meanCheckIn, int varianceCheckIn, int meanSelfCheckIn, int varianceSelfCheckIn, int meanTurvatarkastus, int varianceTurvatarkastus, int meanPortti, int variancePortti, int meanSaapumisvali, int varianceSaapumisvali, double selfCheckInTodennakoisyys) {
         super(kontrolleri);
@@ -53,36 +24,24 @@ public class OmaMoottori extends Moottori {
         this.varianceTurvatarkastus = varianceTurvatarkastus;
         this.meanPortti = meanPortti;
         this.variancePortti = variancePortti;
-        /*
-        this.meanPalveluaika = meanPalveluaika;
-        this.variancePalveluaika = variancePalveluaika;
-        */
         this.meanSaapumisvali = meanSaapumisvali;
         this.varianceSaapumisvali = varianceSaapumisvali;
         this.selfCheckInTodennakoisyys = selfCheckInTodennakoisyys;
-        System.out.println(selfCheckInTodennakoisyys);
+
         checkInPisteet = new Palvelupiste[checkInKoko];
         selfCheckInPisteet = new Palvelupiste[selfCheckInKoko];
         turvatarkastusPisteet = new Palvelupiste[turvatarkastusKoko];
         porttiPisteet = new Palvelupiste[porttiKoko];
 
-
-        // Luo check-in pisteet
         for (int i = 0; i < checkInKoko; i++) {
             checkInPisteet[i] = new CheckIn("Check-in " + (i + 1), new Normal(meanCheckIn, varianceCheckIn), tapahtumalista, TapahtumanTyyppi.CHECK_IN_VALMIS);
         }
-
-        // Luo self-check-in pisteet
         for (int i = 0; i < selfCheckInKoko; i++) {
             selfCheckInPisteet[i] = new SelfCheckIn("Self-check-in " + (i + 1), new Normal(meanSelfCheckIn, varianceSelfCheckIn), tapahtumalista, TapahtumanTyyppi.SELF_CHECK_IN_VALMIS);
         }
-
-        // Luo turvatarkastus pisteet
         for (int i = 0; i < turvatarkastusKoko; i++) {
             turvatarkastusPisteet[i] = new Turvatarkastus("Turvatarkastus " + (i + 1), new Normal(meanTurvatarkastus, varianceTurvatarkastus), tapahtumalista, TapahtumanTyyppi.TURVATARKASTUS_VALMIS);
         }
-
-        // Luo portti pisteet
         for (int i = 0; i < porttiKoko; i++) {
             porttiPisteet[i] = new Portti("Portti " + (i + 1), new Normal(meanPortti, variancePortti), tapahtumalista, TapahtumanTyyppi.PORTTI_VALMIS);
         }
@@ -97,41 +56,47 @@ public class OmaMoottori extends Moottori {
 
     @Override
     protected void suoritaTapahtuma(Tapahtuma t) {
-        Asiakas a;
         switch ((TapahtumanTyyppi) t.getTyyppi()) {
             case ARRIVAL:
-                if (Math.random() < selfCheckInTodennakoisyys) {
-                    getShortestQueue(selfCheckInPisteet).lisaaJonoon(new Asiakas());
-                } else {
-                    getShortestQueue(checkInPisteet).lisaaJonoon(new Asiakas());
-                }
-                saapumisprosessi.generoiSeuraava();
+                handleArrival();
                 break;
             case CHECK_IN_VALMIS:
-                siirraAsiaks(checkInPisteet, turvatarkastusPisteet);
+                siirraAsiakas(checkInPisteet, turvatarkastusPisteet);
                 break;
             case SELF_CHECK_IN_VALMIS:
-                siirraAsiaks(selfCheckInPisteet, turvatarkastusPisteet);
+                siirraAsiakas(selfCheckInPisteet, turvatarkastusPisteet);
                 break;
             case TURVATARKASTUS_VALMIS:
-                siirraAsiaks(turvatarkastusPisteet, porttiPisteet);
+                siirraAsiakas(turvatarkastusPisteet, porttiPisteet);
                 break;
             case PORTTI_VALMIS:
-                for (Palvelupiste p : porttiPisteet) {
-                    if (p.onVarattu() && p.onJonossa() && p.getJono().peek().getPalvelunPaattymisaika() == Kello.getInstance().getAika()) {
-                        a = p.otaJonosta();
-                        a.setPoistumisaika(Kello.getInstance().getAika());
-                        a.raportti();
-                        laskeKeskiProsessiAika(a.getValmiitAsiakkaat(), a.getProsessiAika());
-                        break;
-                    }
-                }
+                handlePorttiValmis();
                 break;
         }
         paivitaGUI();
     }
 
-    private void siirraAsiaks(Palvelupiste[] mista, Palvelupiste[] minne) {
+    private void handleArrival() {
+        if (Math.random() < selfCheckInTodennakoisyys) {
+            getShortestQueue(selfCheckInPisteet).lisaaJonoon(new Asiakas());
+        } else {
+            getShortestQueue(checkInPisteet).lisaaJonoon(new Asiakas());
+        }
+        saapumisprosessi.generoiSeuraava();
+    }
+
+    private void handlePorttiValmis() {
+        for (Palvelupiste p : porttiPisteet) {
+            if (p.onVarattu() && p.onJonossa() && p.getJono().peek().getPalvelunPaattymisaika() == Kello.getInstance().getAika()) {
+                Asiakas a = p.otaJonosta();
+                a.setPoistumisaika(Kello.getInstance().getAika());
+                a.raportti();
+                break;
+            }
+        }
+    }
+
+    private void siirraAsiakas(Palvelupiste[] mista, Palvelupiste[] minne) {
         for (Palvelupiste p : mista) {
             if (p.onVarattu() && p.onJonossa() && p.getJono().peek().getPalvelunPaattymisaika() == Kello.getInstance().getAika()) {
                 Asiakas a = p.otaJonosta();
@@ -163,27 +128,27 @@ public class OmaMoottori extends Moottori {
 
     @Override
     protected void tulokset() {
-        int loppuaika = (int) Kello.getInstance().getAika();
-        int valmiitAsiakkaat = (int) Asiakas.getValmiitAsiakkaat();
-        int meanLapimenoaika = (int) Asiakas.getKeskiViipyminen();
-        int checkInMeanPalveluaika = (int) CheckIn.getKeskiPalveluaika();
-        int checkInKokoPalveluaika = (int) CheckIn.getTotalPalveluaika();
-        int checkInKayttoaste = (int) CheckIn.getKayttoAste();
-        int checkInSuorituskyky = (int) CheckIn.getLapimeno();
-        int selfCheckInMeanPalveluaika = (int) SelfCheckIn.getKeskiPalveluaika();
-        int selfCheckInKokoPalveluaika = (int) SelfCheckIn.getTotalPalveluaika();
-        int selfCheckInKayttoaste = (int) SelfCheckIn.getKayttoAste();
-        int selfCheckInSuorituskyky = (int) SelfCheckIn.getLapimeno();
-        int turvatarkastusMeanPalveluaika = (int) Turvatarkastus.getKeskiPalveluaika();
-        int turvatarkastusKokoPalveluaika = (int) Turvatarkastus.getTotalPalveluaika();
-        int turvatarkastusKayttoaste = (int) Turvatarkastus.getKayttoAste();
-        int turvatarkastusSuorituskyky = (int) Turvatarkastus.getLapimeno();
-        int porttiMeanPalveluaika = (int) Portti.getKeskiPalveluaika();
-        int porttiKokoPalveluaika = (int) Portti.getTotalPalveluaika();
-        int porttiKayttoaste = (int) Portti.getKayttoAste();
-        int porttiSuorituskyky = (int) Portti.getLapimeno();
-
-        Results results = new Results(loppuaika, valmiitAsiakkaat, meanLapimenoaika, checkInMeanPalveluaika, checkInKokoPalveluaika, checkInKayttoaste, checkInSuorituskyky, selfCheckInMeanPalveluaika, selfCheckInKokoPalveluaika, selfCheckInKayttoaste, selfCheckInSuorituskyky, turvatarkastusMeanPalveluaika, turvatarkastusKokoPalveluaika, turvatarkastusKayttoaste, turvatarkastusSuorituskyky, porttiMeanPalveluaika, porttiKokoPalveluaika, porttiKayttoaste, porttiSuorituskyky);
+        Results results = new Results(
+                (int) Kello.getInstance().getAika(),
+                (int) Asiakas.getValmiitAsiakkaat(),
+                (int) Asiakas.getKeskiViipyminen(),
+                (int) CheckIn.getKeskiPalveluaika(),
+                (int) CheckIn.getTotalPalveluaika(),
+                (int) CheckIn.getKayttoAste(),
+                (int) CheckIn.getLapimeno(),
+                (int) SelfCheckIn.getKeskiPalveluaika(),
+                (int) SelfCheckIn.getTotalPalveluaika(),
+                (int) SelfCheckIn.getKayttoAste(),
+                (int) SelfCheckIn.getLapimeno(),
+                (int) Turvatarkastus.getKeskiPalveluaika(),
+                (int) Turvatarkastus.getTotalPalveluaika(),
+                (int) Turvatarkastus.getKayttoAste(),
+                (int) Turvatarkastus.getLapimeno(),
+                (int) Portti.getKeskiPalveluaika(),
+                (int) Portti.getTotalPalveluaika(),
+                (int) Portti.getKayttoAste(),
+                (int) Portti.getLapimeno()
+        );
 
         kontrolleri.naytaLoppuaika(Kello.getInstance().getAika());
         kontrolleri.setResults(results);
@@ -216,11 +181,7 @@ public class OmaMoottori extends Moottori {
             }
         }
         double kayttoaste = ((double) varatut / pisteet.length) * 100;
-
-        if (kayttoaste > 100) {
-            return 100;
-        }
-        return ((double) varatut / pisteet.length) * 100;
+        return Math.min(kayttoaste, 100);
     }
 
     private void paivitaGUI() {
@@ -235,14 +196,6 @@ public class OmaMoottori extends Moottori {
 
         porttiAsiakasMaara = getAsiakasMaara(porttiPisteet);
         kontrolleri.visualisoiAsiakas(4, porttiAsiakasMaara, getKayttoaste(porttiPisteet), porttiPisteet.length);
-    }
-
-    private void laskeKeskiProsessiAika(double valmiitAsiakkaat, double prosessiAika) {
-        this.prosessiAika += prosessiAika;
-        double keskiProsessiAika = this.prosessiAika / valmiitAsiakkaat;
-        System.out.println();
-        System.out.println("Asiakkaiden läpimenoaikojen keskiarvo tähän asti: " + keskiProsessiAika);
-        System.out.println();
     }
 
 }
